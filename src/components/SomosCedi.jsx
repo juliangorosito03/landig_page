063bed751f9e482855c6e909a3451a05ace9c9bb
JulianAgustinGorosito <javagorosito@gmail.com>
2025-04-28 13:49:01 -0300

    sigo con problemas en la ruta

--- src/components/SomosCedi.jsx
+++ src/components/SomosCedi.jsx
@@ -4,13 +4,14 @@ function SomosCedi() {
   return (
     <section id="somos-grupo-cedi" className="w-full py-16 bg-[#0e0e0e] text-white flex flex-col items-center">
       
-      <h2 className="text-4xl font-bold mb-8"></h2>
+      <h2 className="text-4xl font-bold mb-8" data-aos="fade-up">
+      </h2>
 
-      <div className="flex justify-center mt-4">
+      <div className="flex justify-center mt-4" data-aos="fade-up" data-aos-delay="100">
         <img 
           src="/GrupoCedi.png" 
           alt="Logo Grupo CEDI" 
-          className="h-24 w-auto" 
+          className="h-24 md:h-32 w-auto object-contain" 
         />
       </div>
 
